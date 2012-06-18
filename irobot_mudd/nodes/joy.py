667fb407ae251a8bbe0c8b1303b6d7555f430633
Strelka <hmcrobotlab@gmail.com>
2012-06-18 10:44:54 -0700

    joystick create control

---
+++ irobot_mudd/nodes/joy.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import roslib; roslib.load_manifest('irobot_mudd')
+from irobot_mudd.srv import *
+from irobot_mudd.msg import *
+import rospy
+import pygame
+
+rospy.wait_for_service("tank")
+heli = rospy.ServiceProxy("droneControl", Tank)
+print "connected"
+
+pygame.init()
+j = pygame.joystick.Joystick(0)
+j.init()
+
+while True:
+    pygame.event.pump()
+
+    flag = 0
+    x = j.get_axis(0)
+    theta = j.get_axis(1)
+
+    if abs(x) < .17:
+      x = 0
+    if abs(theta) < .17:
+      theta = 0
+
+    speedl = (x * 250) + (theta * 250)
+    speedr = (x * 250) - (theta * 250)
+    tank(speedl,speedr)
+    #rospy.sleep(.01)
+
+
