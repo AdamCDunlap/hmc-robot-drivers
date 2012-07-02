233012b2e8192859b2b5448f4811d7041d469d82
Strelka <hmcrobotlab@gmail.com>
2012-07-02 13:34:11 -0700

    ARdrone2 driver first commit.  It compiles through ros

---
+++ ardrone2_mudd/build/CMakeFiles/driver.dir/DependInfo.cmake
@@ -0,0 +1,15 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/robotics/ros_workspace/hmc-robot-drivers/ardrone2_mudd/src/Navdata/navdata.cpp" "/home/robotics/ros_workspace/hmc-robot-drivers/ardrone2_mudd/build/CMakeFiles/driver.dir/src/Navdata/navdata.o"
+  "/home/robotics/ros_workspace/hmc-robot-drivers/ardrone2_mudd/src/Video/video_stage.cpp" "/home/robotics/ros_workspace/hmc-robot-drivers/ardrone2_mudd/build/CMakeFiles/driver.dir/src/Video/video_stage.o"
+  "/home/robotics/ros_workspace/hmc-robot-drivers/ardrone2_mudd/src/driver.cpp" "/home/robotics/ros_workspace/hmc-robot-drivers/ardrone2_mudd/build/CMakeFiles/driver.dir/src/driver.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ ardrone2_mudd/build/CMakeFiles/driver.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/driver.dir/src/driver.o"
+  "CMakeFiles/driver.dir/src/Navdata/navdata.o"
+  "CMakeFiles/driver.dir/src/Video/video_stage.o"
+  "../bin/driver.pdb"
+  "../bin/driver"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/driver.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
