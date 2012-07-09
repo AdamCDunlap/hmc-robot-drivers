1dd12950311d948d77e81bdcd944d4d65e56a639
Strelka <hmcrobotlab@gmail.com>
2012-07-09 09:54:58 -0700

    Forget to add src files

---
+++ ardrone2_mudd/src/navdata.h
@@ -0,0 +1,11 @@
+#ifndef _NAVDATA_H_
+#define _NAVDATA_H_
+
+#include "driver.h"
+extern "C" {
+#include <ardrone_tool/Navdata/ardrone_navdata_client.h>
+}
+
+extern int state;
+
+#endif // _NAVDATA_H_
