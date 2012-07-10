b8a6332838d69413865a46078c312f74dd6db431
Strelka <hmcrobotlab@gmail.com>
2012-07-10 16:28:34 -0700

    tf broadcaster for the kinect pan

---
+++ kinect_pan/bin/kinecttf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import roslib; roslib.load_manifest('kinect_pan')
+import rospy
+import tf
+from dynamixel_msgs import msg
+
+
+def jcb(data):
+  br = tf.TransformBroadcaster()
+  br.sendTransform((0,0,0),
+      tf.transformations.quaternion_from_euler(0,0,data.current_pos),
+      rospy.Time.now(),
+      "pan_base",
+      "openni_camera")
+
+if __name__ == '__main__':
+  rospy.init_node("kinecttf")
+  rospy.Subscriber("/pan_controller/state", msg.JointState,jcb)
+  print "spinning"
+  rospy.spin()
