d02a9a870716c83f7346d7db035a81b7f8da1d8f
arnold <arnold@robotLab>
2012-08-01 17:07:01 -0700

    proportional targeted landing closer to working

--- ardrone2_mudd/nodes/targetLanding.py
+++ ardrone2_mudd/nodes/targetLanding.py
@@ -14,94 +14,62 @@ import ardrone2
 #########################################
 import time,sys,random,cv,cv_bridge,math
 
-#########################################
-#########    Variable Setting   #########
-#########################################
-BOUNDING_DATA_SOURCE = "imageData"
-NAV_DATA_SOURCE = "ardrone2/navData"
-DRONE_CONTROL_SOURCE = "ardrone2/heli"
-DRONE_CONFIG_SOURCE = "ardrone2/config"
+IMAGE_SOURCE = "imageData"
 
-class myArdrone(ardrone.Ardrone):
-    def __init__(self, controlName=DRONE_CONTROL_SOURCE, ):
+class myArdrone(ardrone2.Ardrone):
+    def __init__(self):
         ardrone2.Ardrone.__init__(self)
+        self.toleranceArea = 0.05
         self.state = "keyboard"
-        self.boxX = 0
-        self.boxHeight = 0
-        self.tarHeight = 35.0/240
-        self.tarX = .46
-	self.toleranceX = 40/320.0
-	self.toleranceHeight = 10/240.0
-	self.toleranceArea = .001
-	self.noBox = True
+        self.noBox = True
+        self.tracking = False
+        self.vertMult = 0.5
+        self.strafeMult = 0.5
+        self.altMult = 1.0
+        self.setCam(1) # Setting downard camera
 
     def loop(self):
         while True:
             char = self.getKeyPress()
             if char == 'n':
                 self.state = "start"
-                self.fsm()
+                self.tracking = True
+                self.trackingBrain()
+            elif self.tracking and not self.noBox:
+                self.trackingBrain()
 
-    def fsm(self,timer_event=None):
-        print self.state + " " + str(self.boxX) + " " + str(self.boxHeight)
-        if self.state == "keyboard":
-            return
-        elif self.state == "start":
-            self.takeoff()
-            self.state = "searching"
-	elif self.state == "searching":
-	    if self.noBox:
-	    	self.spinLeft(.3)
-	    else:
-		self.state = "closing_in"
-        elif self.state == "closing_in":
-            if abs(self.boxX - self.tarX) < self.toleranceX:
-                self.state = "approaching"
-            elif self.boxX < self.tarX:
-                self.spinLeft(.3)
-            else:
-                self.spinRight(.3)
-        elif self.state == "approaching":
-	    if self.noBox:
-		self.state = "searching"
-            elif abs(self.boxX - self.tarX) > self.toleranceX:
-                self.state = "closing_in"
-            elif self.tarHeight - self.boxHeight > self.toleranceHeight:
-                self.forward(.07)
-            else:
-                self.land()
-                self.state = "landing"
-        elif self.state == "landing":
-            self.hover()
-            rospy.sleep(.25)
-            self.land()
-            self.state = "keyboard"
-
-        rospy.Timer( rospy.Duration(0.01), self.fsm, oneshot=True )
-
+    def trackingBrain(self,timer_event=None):
+        if height > 0.85:
+            self.land
+            self.tracking = False
+        dist = math.sqrt(self.centerX**2 + self.centerY**2)
+        downPower = self.altMult/(1.0 + dist)
+        leftRightPower = (self.centerX - .5) * 2.0 * self.strafeMult
+        forwardBackPower = (self.centerY - .5) * 2.0 * self.vertMult
+        self.forward(forwardBackPower)
+        self.strafeRight(leftRightPower)
+        self.down(downPower)
+        
+       
     def bBoxUpdate(self, data):
         #Format is: "CenterX (in percent of box width) CenterY (in percent of box height)
         #            Area (in percent of box area) LeftEdge (in percent of box width)
         #            TopEdge (in percent of box height) RightEdge (in percent of box width)
         #            BottomEdge (in percent of box height)"
-        br      = data.data.split()
-	centerX = float(br[0])
-	centerY = float(br[1])
-	area    = float(br[2])
-	height  = float(br[6])-float(br[4]) #the y axis points down.
-        if area < self.toleranceArea:
-	    print "I see a too small box of area %f"%area
-	    self.noBox = True
-            self.boxX = float("inf")
-	    self.boxHeight = 0
+        br           = data.data.split()
+        self.centerX = float(br[0])
+        self.centerY = float(br[1])
+        self.area    = float(br[2])
+        self.height  = float(br[6])-float(br[4]) #the y axis points down.
+        if self.area < self.toleranceArea:
+            print "I see a too small box of area %f" % self.area
+            self.noBox = True
         else:
-	    self.noBox = False
-            self.boxX = centerX
-            self.boxHeight = height
+            self.noBox = False
 
 if __name__== "__main__":
   drone = myArdrone()
   print "\r Connecting to bounding box data"
-  ardrone.rospy.Subscriber("imageData",String,drone.bBoxUpdate,queue_size=1)
+  rospy.Subscriber(IMAGE_SOURCE,String,drone.bBoxUpdate,queue_size=1)
   print "Ready"
   drone.loop()
